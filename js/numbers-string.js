9eeab2f2d86d6d2d5ddca270fbaf9e74f56a5910
Yurii04UA <uyriparaka@gmail.com>
2021-11-14 15:14:31 +0100

    js study

---
+++ js/numbers-string.js
@@ -0,0 +1,24 @@
+Number;
+5,15,11,3.15,-18;
+let a = 5;
+
+// console.log(a+3);
+
+// console.log(`How much? ${a}`)  - в одинарных скобках `` записуют если нужно добавить переменную в строку. Переменная записывается так ${a}
+
+String;
+"Hello", "Hello world";
+let b = "Hello my world"
+
+// console.log(b);
+
+b.length //- длина строки
+// console.log(b.length)
+
+ b.slice //- показывает значение в диапозоне
+
+//  console.log(b.slice(0,3)) - показывает значения от 0 знака до 3 знака
+//  =Hel;
+
+//  console.log(b.slice(7)) -показывает начиная со 7 знака
+//  =y world; 
