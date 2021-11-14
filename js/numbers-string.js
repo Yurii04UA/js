6f93c84809d06a58ba45f729d185b820a8a163cb
Yurii04UA <uyriparaka@gmail.com>
2021-11-14 17:04:50 +0100

    study

--- js/numbers-string.js
+++ js/numbers-string.js
@@ -1,15 +1,26 @@
 Number;
 5,15,11,3.15,-18;
 let a = 5;
-let gg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let gg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11];
+
 Math.random // случайное число от 0 до 1
+
+function ran(min, max){   //случайное число в диапозоне
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}   
+
+console.log(ran(1,500))  //случайное число в диапозоне
+
+
 Math.max(1,5,15,11,-11) // максимальное число из перечисленных
 console.log(Math.random())
-console.log(Math.max.apply(null, gg))
-console.log(Math.min.apply(null, gg))
+console.log(Math.max.apply(null, gg)) //макс число из масива
+console.log(Math.min.apply(null, gg)) //мин число из масива
 
-console.log(gg.length)
-// console.log(a+3);
+// gg[11] = 19;
+// console.log(gg)
+// console.log(Math.max.apply(null, gg))
 
 // console.log(`How much? ${a}`)  - в одинарных скобках `` записуют если нужно добавить переменную в строку. Переменная записывается так ${a}
 
