13b6cc274587b91f1307c5228e895a678fdad313
Yurii04UA <uyriparaka@gmail.com>
2021-11-15 17:54:17 +0100

    js

---
+++ js/prompt.js
@@ -0,0 +1,9 @@
+let firstName = prompt("What is your name?")
+let lastName = prompt("What is your last name?")
+let age = prompt ("How old are you?")
+let fullName = `User : ${firstName} ${lastName}`
+let userAge = `Your age : ${age}`
+
+console.log(fullName)
+
+console.log(userAge)
