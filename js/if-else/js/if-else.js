591c56b3b763319115dcc64c8d004210482c7147
Yurii04UA <uyriparaka@gmail.com>
2021-11-16 18:10:01 +0100

    js

--- js/if-else/js/if-else.js
+++ js/if-else/js/if-else.js
@@ -1,18 +1,37 @@
-let height = prompt("Enter your height (cm)");
-let userHeight = `Your height : ${height} cm`;
-let weight = prompt("Enter your weight (kg)");
-let userWeight = `Your weight : ${weight} kg` 
-
-let indexWeightBody = (weight /(height**2))*10000;
-let indexWeightBodyFix = indexWeightBody.toFixed(1);
-
-if (indexWeightBodyFix < 25){
-    console.log("Your index weight body = "+ indexWeightBodyFix + " kg/m2")
-    console.log("You are the normal weight =)")
-} else if(indexWeightBodyFix > 25) {
-    console.log("Your index weight body = "+ indexWeightBodyFix + " kg/m2")
-    console.log("You are overweight =(")
+let a = 5
+if(a < 4 ){
+    console.log("Yes 5 < 4")
+} else{
+    console.log("No 5 > 4")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let height = prompt("Enter your height (cm)");
+// let userHeight = `Your height : ${height} cm`;
+// let weight = prompt("Enter your weight (kg)");
+// let userWeight = `Your weight : ${weight} kg` 
+
+// let indexWeightBody = (weight /(height**2))*10000;
+// let indexWeightBodyFix = indexWeightBody.toFixed(1);
+
+// if (indexWeightBodyFix < 25){
+//     console.log("Your index weight body = "+ indexWeightBodyFix + " kg/m2")
+//     console.log("You are the normal weight =)")
+// } else if(indexWeightBodyFix > 25) {
+//     console.log("Your index weight body = "+ indexWeightBodyFix + " kg/m2")
+//     console.log("You are overweight =(")
+// }
     
 
 
