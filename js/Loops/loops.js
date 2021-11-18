754dca357f9898fd97f429bfdcc4563884711195
Yurii04UA <uyriparaka@gmail.com>
2021-11-18 16:07:42 +0100

    js

---
+++ js/Loops/loops.js
@@ -0,0 +1,24 @@
+/////////// while
+
+// let x = 1;
+
+// while(x < 10){
+//     console.log(x);
+//     x++; 
+// }
+////// x++; - значение х+1
+////// х+=2 - значение х+2
+
+let string = "Hello world!";
+let count = 0;
+
+// while( count < string.length){
+//     console.log(string[count]);
+//     count++;
+// }
+
+// while( count < string.length){
+//     console.log(string.slice(count));
+//     count++;
+// }
+
--- js/switch/switch.js
+++ js/switch/switch.js
@@ -1,47 +1,76 @@
-//  if else
+// //////////  if else
 
-let age = 32;
-let groupNumber;
+// let age = 32;
+// let groupNumber;
 
-if (age >= 18 && age <=26){
-    groupNumber = 1;
-}else if(age > 26 && age <= 46){
-    groupNumber = 2;
-} else{
-    groupNumber = 3;
-}
+// if (age >= 18 && age <=26){
+//     groupNumber = 1;
+// }else if(age > 26 && age <= 46){
+//     groupNumber = 2;
+// } else{
+//     groupNumber = 3;
+// }
 
-console.log(groupNumber);
+// console.log(groupNumber);
 
-// switch
+// // switch
+
+// switch(true){
+//     case age >= 18 && age <= 26:
+//         groupNumber = 1;
+//         break;
+//     case age > 26 && age <=46 :
+//         groupNumber = 2;
+//         break;
+//     default :
+//         groupNumber = 3;
+// }
+
+// console.log(groupNumber);
+
+// ////////////// switch делает те же функии что и if else(кому как удобнее)
+
+// let education = "html"
+
+// switch(education){
+//     case "css" :
+//         console.log("Yes you study css");
+//         break;
+//     case "javascript" :
+//         console.log("Yes you study JS")
+//         break;
+//     case "html" :
+//         console.log("Yes you study html")
+//         break;
+//     default :
+//         console.log("You study somefing")
+// }
+
+
+let userName = prompt("Enter is your name...");
+let userPass = prompt("Enter is your password...");
+
+const userYurii = "Yurii";
+const userPassYurii = "1111";
+
+const userMax = "Max";
+const userPassMax = "3333";
+
+
+const userAdmin = "Admin";
+const userPassAdmin = "Admin";
 
 switch(true){
-    case age >= 18 && age <= 26:
-        groupNumber = 1;
+    case userName == userYurii && userPass == userPassYurii :
+        alert("Hello " + userName );
         break;
-    case age > 26 && age <=46 :
-        groupNumber = 2;
+    case userName == userMax && userPass == userPassMax :
+        console.log("Hello " + userName );
         break;
-    default :
-        groupNumber = 3;
-}
-
-console.log(groupNumber);
-
-// switch делает те же функии что и if else(кому как удобнее)
-
-let education = "html"
-
-switch(education){
-    case "css" :
-        console.log("Yes you study css");
-        break;
-    case "javascript" :
-        console.log("Yes you study JS")
-        break;
-    case "html" :
-        console.log("Yes you study html")
+    case userName == userAdmin && userPass == userAdmin :
+        console.log("Hello " + userName + " you must fix");
         break;
     default :
-        console.log("You study somefing")
+        alert("Your login or password not correct")
 }
+
