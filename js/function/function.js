3cfbccb529951f38c5ab0478f7ed9f025de7531b
Yurii04UA <uyriparaka@gmail.com>
2021-11-20 00:42:05 +0100

    fucntion

---
+++ js/function/function.js
@@ -0,0 +1,13 @@
+arr = [11,23,45,64,546,1,-11]
+
+
+function func(name, age){
+    console.log("hello " + name  + " your age : "+ age );
+}
+
+func("Yurii", 21);
+func("Yurii", 22);
+func("Yurii", 25);
+func("Yurii", 28);
+
+
