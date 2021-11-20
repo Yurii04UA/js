16b73fef1a6cc94b81f867aa67f0fdbcf1097daa
Yurii04UA <uyriparaka@gmail.com>
2021-11-20 22:19:34 +0100

    js

--- js/function/function.js
+++ js/function/function.js
@@ -21,3 +21,45 @@ arr = [11,23,45,64,546,1,-11]
 // console.log(a)
 
 /////////////////////////////////
+
+
+/* TypesData:
+String;
+Number
+bigN
+boolean (true, false)
+undefinde
+null
+obj
+array
+
+Array:
+arr= [10,"name",true, obj, arr2[]]
+
+Obj;
+
+obj={ key, value}
+obj2 = { name: "Yurii", age:18}
+
+
+Switch
+if else
+
+let a =1;
+if (a < 0){
+    console.log("no")
+} else if(a == 0 ){
+    console.log("Yes a= 0")
+}else{
+    console.log("a>0")
+}
+let name = "Yurii"
+
+switch(name){
+    case "Stas" : console.log("Yes your name Stas");
+    break
+    case "Yurii" : console.log("Yes your name Yurii");
+    break
+}
+
+*/
