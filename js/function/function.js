947b2d0d82ec3aadf05c6dd9398f6e646a77a37d
Yurii04UA <uyriparaka@gmail.com>
2021-11-20 01:02:46 +0100

    fucntion

--- js/function/function.js
+++ js/function/function.js
@@ -1,13 +1,23 @@
 arr = [11,23,45,64,546,1,-11]
 
+    // let userName = prompt("What is your name?")
+    // let userAge = prompt("How old are you?")
+// function func(name, age){
+//     console.log("hello " + name  + " your age : "+ age );
+    
+// }
 
-function func(name, age){
-    console.log("hello " + name  + " your age : "+ age );
-}
+// func("Yurii", 22);
+// func(userName, userAge);
 
-func("Yurii", 21);
-func("Yurii", 22);
-func("Yurii", 25);
-func("Yurii", 28);
+///////////////////////////
+//     let userName = prompt("What is your name?")
+//     let userAge = prompt("How old are you?")
+// function func(name,age){
+//     return "user : "+name+ " age : "+age  
+// }
 
+// let a = func(userName,userAge);
+// console.log(a)
 
+/////////////////////////////////
