3bf61c3310b86d0b719e217387b0e1d4ee0e59ba
Yurii04UA <uyriparaka@gmail.com>
2021-12-19 01:50:29 +0200

    s

---
+++ index.js
@@ -0,0 +1,72 @@
+// const x = document.querySelector(".fuck")
+
+// x.addEventListener("click", function(){
+//     console.log("Pii")
+// })
+
+
+
+/*
+// Types date
+1.number 
+2,12,-2,3.15
+2.String
+let a = "String"
+let b = "5"
+3.Boolean
+true,false
+4.bigl очень большие числа
+5.null - пусто нечего нет
+6.undefined - еще не назначено
+7. obj
+8.Symbol
+
+
+Условыне опереторы сравнения
+if else
+switch
+
+Loops
+
+for
+while
+*/
+
+/////////////////////////////////////
+
+// `use script`
+// console.log(a)
+// var a = "a";
+// function b(){
+//     console.log(a);
+//    var a = "a1";
+//     console.log(a)
+// }
+// b()
+
+
+// console.log(q)
+// let q = "q1";
+// function w (){
+//     console.log(q)
+//     q = "q1"
+//     console.log(q)
+// }
+///////////////////////////////////////////
+
+// `use script`
+// console.log(a)
+// var a = "a";
+// var as = "sd";
+// function b(){
+//     console.log(a);  //если отставить "а" будет андефаинд если "ас" то выведет 
+//    var a = "a1";
+//     console.log(a)
+// }
+// b()
+//  
+
+`use script`
+
+let b
+console.log(b)
