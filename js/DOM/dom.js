f5a799702d7b3785df49a66bee21de2515899be2
Yurii04UA <uyriiparaka@gmail.com>
2021-12-25 23:02:13 +0200

    learning js

--- js/DOM/dom.js
+++ js/DOM/dom.js
@@ -1,8 +1,24 @@
-// document.getElementById()
-// document.getElementsByClassName()
+// document.getElementById()  -ищет по айди
+// document.getElementsByClassName() -ищет все элем с таким классом 
+// document.getElementsByTagName() -ищет по тегам(найдет все <p>)
+// document.querySelector() - ищет первое совпадение по классу (".class") по айди("#id") по тегу ("tag")
+// document.querySelectorAll() - ищет все совпадения по классу (".class") по айди("#id") по тегу ("tag")
 
 // document.getElementById("test").innerHTML = "New value";  // . innereHTML -заменяет содержимое 
 
-let x = document.getElementsByClassName("firstThreeEl");
-console.log(x)
-document.getElementById("intro").innerHTML = "new text with (index0) :" + x[0].innerHTML;
+// let x = document.getElementsByClassName("firstThreeEl");
+// console.log(x)
+
+// document.getElementById("intro").innerHTML = "new text with (index0) :" + x[0].innerHTML;
+
+// let a = document.getElementsByTagName("p") 
+// console.log(a)
+
+// document.getElementById("intro1").innerHTML = 'Some text + first paragraph ' + a[0].innerHTML
+
+// let x = document.querySelector(".firstThreeEl").innerHTML;
+// console.log(x);
+
+let a = document.querySelectorAll(".firstThreeEl")
+
+console.log(a[2].innerHTML)
