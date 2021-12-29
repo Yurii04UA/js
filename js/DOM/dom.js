da75c5fcfb29d29b3774f4d9c07163372d888c49
Yurii04UA <uyriparaka@gmail.com>
2021-12-29 14:08:40 +0200

    js

--- js/DOM/dom.js
+++ js/DOM/dom.js
@@ -3,6 +3,15 @@
 
 // document.getElementById("test").innerHTML = "New value";  // . innereHTML -заменяет содержимое 
 
-let x = document.getElementsByClassName("firstThreeEl");
-console.log(x)
-document.getElementById("intro").innerHTML = "new text with (index0) :" + x[0].innerHTML;
+// let x = document.getElementsByClassName("firstThreeEl");
+// console.log(x)
+// document.getElementById("intro").innerHTML = "new text with (index0) :" + x[0].innerHTML;
+
+
+/////////////////////
+// изменение стилей с помошью js    
+let a = document.querySelector("#thridElem");
+console.log(a.innerHTML)
+a.style.color = "red";
+a.style.fontSize = "55px";
+a.style.background = "yellow"
