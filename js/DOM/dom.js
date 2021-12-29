610e126e5ba7d64e8d14f806b535b2c20b4e0e38
Yurii04UA <uyriparaka@gmail.com>
2021-12-29 15:04:47 +0200

    js

--- js/DOM/dom.js
+++ js/DOM/dom.js
@@ -10,8 +10,38 @@
 
 /////////////////////
 // изменение стилей с помошью js    
-let a = document.querySelector("#thridElem");
-console.log(a.innerHTML)
-a.style.color = "red";
-a.style.fontSize = "55px";
-a.style.background = "yellow"
+// let a = document.querySelector("#thridElem");
+// console.log(a.innerHTML)
+// a.style.color = "red";
+// a.style.fontSize = "55px";
+// a.style.background = "yellow"
+
+// const fuck = document.querySelector(".fuck");
+// console.log(fuck)
+// fuck.classList.add("style")
+//////////////////////////////////////////////////
+// поменять сожержимое атрибута
+
+// let img = document.querySelector(".img1");
+// img.getAttribute("src")
+// img.setAttribute("src","././../../images/img2.jpg")
+
+// let link = document.querySelector(".link");
+// link.setAttribute("href","https://www.youtube.com")
+// //////////////////////////////
+// EVENT
+let h1 = document.querySelector(".h1");
+h1.addEventListener("click",function(){
+    h1.style.color = "pink"
+})
+h1.addEventListener("click",function(){
+    h1.style.background = "black"
+})
+
+let liElem = document.getElementsByTagName("li");
+for(i=0;i<liElem.length;i++){
+    liElem[i].addEventListener("click",function(){
+        this.style.color = "green"
+    })
+}
+
