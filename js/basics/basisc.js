9dd540021980d0dff91024a715f296ca73d50b7e
Yurii04UA <uyriparaka@gmail.com>
2022-01-06 17:00:41 +0200

    js

---
+++ js/basics/basisc.js
@@ -0,0 +1,8 @@
+let userBodyWeight = prompt("Enter your weight (kg)");
+let userHeight = prompt("Enter your height (cm)");
+let userIndexBodyWeight = Math.round((userBodyWeight / (userHeight*2))*100);
+
+if (userBodyWeight == null){
+   console.log("ss")
+   
+}
