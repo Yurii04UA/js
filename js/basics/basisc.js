26d19e938a37ed2c19a7b88a530a95ae4e7ec101
Yurii04UA <uyriparaka@gmail.com>
2022-01-07 18:48:06 +0200

    js

--- js/basics/basisc.js
+++
@@ -1,8 +0,0 @@
-let userBodyWeight = prompt("Enter your weight (kg)");
-let userHeight = prompt("Enter your height (cm)");
-let userIndexBodyWeight = Math.round((userBodyWeight / (userHeight*2))*100);
-
-if (userBodyWeight == null){
-   console.log("cancel")
-   
-}
