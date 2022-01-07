7f8070539c5f5c0c815b14d3f33508a3bfbee5cb
Yurii04UA <uyriiparaka@gmail.com>
2022-01-07 21:43:32 +0200

    js

--- js/basics/basisc.js
+++ js/basics/basisc.js
@@ -1,1 +1,11 @@
+"use strict";
 
+let hasCertificat = false;
+
+const passExam = true;
+
+if(passExam) hasCertificat = true;
+if(hasCertificat) console.log("wow!")
+
+// const interface = "po"
+// const package = "ss"
