97a5e681244aeabc317039e99015541082bee02e
Yurii04UA <uyriparaka@gmail.com>
2022-01-13 16:11:57 +0200

    quess

---
+++ js/guess the namber/index.js
@@ -0,0 +1,16 @@
+const promptMessage = document.querySelector('.prompt');
+
+const btn = document.querySelector('.start');
+
+const input = document.querySelector('.input_number');
+
+
+btn.addEventListener('click', ()=>{
+   promptMessage.innerHTML = 'well done! you did it';
+   document.body.style.backgroundColor = "#1e994af2"
+});
+
+
+
+
+
