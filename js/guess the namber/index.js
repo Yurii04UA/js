fed3794b799125f8c248236e090b76a4f297ef41
Yurii04UA <uyriparaka@gmail.com>
2022-01-15 01:40:55 +0200

    js

--- js/guess the namber/index.js
+++ js/guess the namber/index.js
@@ -1,16 +1,22 @@
 const promptMessage = document.querySelector('.prompt');
 
-const btn = document.querySelector('.start');
+//buttons
+const btnStart = document.querySelector('.start');
+const btnCheck = document.querySelector('.check');
 
 const input = document.querySelector('.input_number');
+const secretNumber = document.querySelector('#secret_number');
+// btn.addEventListener('click', ()=>{   /// check of working
+//    promptMessage.innerHTML = 'well done! you did it';
+//    document.body.style.backgroundColor = "#1e994af2";
+// });
 
-
-btn.addEventListener('click', ()=>{
-   promptMessage.innerHTML = 'well done! you did it';
-   document.body.style.backgroundColor = "#1e994af2"
+btnCheck.addEventListener('click', () => {
+   const quessingNumber = +input.value;
+   
+   if(!quessingNumber){
+      promptMessage.innerHTML = 'Insert the number'
+   }
+   
 });
-
-
-
-
 
