fffe415c3a9f4324710556b54d617384feb0c4ce
Yurii04UA <uyriparaka@gmail.com>
2022-01-17 18:19:02 +0200

    js

--- js/guess the namber/index.js
+++ js/guess the namber/index.js
@@ -1,16 +1,14 @@
 const promptMessage = document.querySelector('.prompt');
 
 //buttons
-const btnStart = document.querySelector('.start');
+const btnAgain = document.querySelector('.start');
 const btnCheck = document.querySelector('.check');
 
 const input = document.querySelector('.input_number');
 const secretNumber = document.querySelector('#secret_number');
 
-function randomNumberFunction(){
-   return Math.round(Math.random()*100+1)
-};
-const randomNumber = randomNumberFunction()
+
+let randomNumber = Math.round(Math.random()*100+1)
 console.log(randomNumber)
 
 let tryNumb = 10;
@@ -18,36 +16,13 @@ const showScore = document.querySelector('#score');
 showScore.innerHTML = tryNumb
 
 const BestResult = document.querySelector('#result');
+let highscore = 10;
 
 
 // btn.addEventListener('click', ()=>{   /// check of working
 //    promptMessage.innerHTML = 'well done! you did it';
 //    document.body.style.backgroundColor = "#1e994af2";
 // });
-
-
-   
-// btn again
-// btnStart.addEventListener('click',() => {
-//    window.location.reload();
-// })
-btnStart.addEventListener('click',() => {
-   promptMessage.innerHTML = 'start quessing';
-   // try
-   tryNumb = 10;
-   showScore.innerHTML = tryNumb;
-   // best score
-   BestResult.innerHTML = 0;
-
-   //style
-   document.querySelector('.secret_number').style.width = '200px';
-   document.body.style.backgroundColor = '#000000de';
-   secretNumber.innerHTML = '???';
-
-   //random numb
-   randomNumberFunction()
-});
-
 
  // game
 btnCheck.addEventListener('click', () => {
@@ -60,9 +35,13 @@ btnCheck.addEventListener('click', () => {
       } else if (guessingNumber == randomNumber){
          promptMessage.innerHTML = 'Yes, you are quite right. Well done';
          secretNumber.innerHTML = randomNumber;
-         BestResult.innerHTML = 11 - tryNumb;
+         showScore.innerHTML = --tryNumb;
          document.body.style.backgroundColor = '#1e994af2';
          document.querySelector('.secret_number').style.width = '300px';
+         if(highscore > 10 - tryNumb){
+            highscore = 10 - tryNumb
+            BestResult.innerHTML = highscore;
+         }
 
          // game over
       } else if(tryNumb <= 1){
@@ -70,7 +49,7 @@ btnCheck.addEventListener('click', () => {
          document.body.style.backgroundColor = 'rgba(145, 53, 11, 0.95)';
          showScore.innerHTML = --tryNumb;
          promptMessage.innerHTML = 'You didnt guess, start over';
-         btnStart.innerHTML = 'Again'
+         btnAgain.innerHTML = 'Again'
          // setTimeout(()=>window.location.reload(),3000)
       }else if (guessingNumber > randomNumber){
          promptMessage.innerHTML = 'No,secret number <span style = "color: red">less</span> than';
@@ -80,6 +59,20 @@ btnCheck.addEventListener('click', () => {
          showScore.innerHTML = --tryNumb
          
       }
-      
 });
 
+// btn again
+btnAgain.addEventListener('click',() => {
+   promptMessage.innerHTML = 'start quessing';
+   // try
+   tryNumb = 10;
+   showScore.innerHTML = tryNumb;
+   // style
+   document.querySelector('.secret_number').style.width = '200px';
+   document.body.style.backgroundColor = '#000000de';
+   secretNumber.innerHTML = '???';
+   input.value = '';
+   // random number
+   randomNumber = Math.round(Math.random()*100+1);
+   console.log(randomNumber) 
+});
