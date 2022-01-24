46b02a9cdbc40c749b49255cd125354f66708900
Yurii04UA <uyriparaka@gmail.com>
2022-01-24 20:08:57 +0200

    js

--- js/destructuring/des.js
+++ js/destructuring/des.js
@@ -5,9 +5,15 @@ const japaneseRestaurant = {
     location: '108 Midl street, London',
     categories: ['Japaneseeee', 'Sushiii', 'Vegetarion', 'Organic'],
     appetizers: ['Seaweed salad', 'Tempura', 'Edamame', 'Sushi org'],
-    mainMenu: ['Sushi', 'Ramen', 'Tempura']
+    mainMenu: ['Sushi', 'Ramen', 'Tempura'],
+    oderFood: function(appetizersIndex, mainMenuIndex){
+        return [this.appetizers[appetizersIndex] , this.mainMenu[mainMenuIndex]]
+    },
 };
-
+console.log(japaneseRestaurant.oderFood(1,1));
+// Возвращаем значение из функции
+const [appetizersOder, mainMenuOder] = japaneseRestaurant.oderFood(1,1);
+console.log(appetizersOder, mainMenuOder)
 // const arr = [1, 2, 3];
 // const x1 = arr[0];
 // const x2 = arr[1];
@@ -18,5 +24,21 @@ const japaneseRestaurant = {
 // console.log(a1, a2, a3);
 
 // const [categorie1, categorie2] = japaneseRestaurant.categories; // распаковка первых двук
-const [categorie1, , categorie3] = japaneseRestaurant.categories; // распаковка первый и третий
-console.log(categorie1, categorie3)
+let [japanese, , vegetarion] = japaneseRestaurant.categories; // распаковка первый и третий
+console.log(japanese, vegetarion);
+
+//// Мы можем менять элементы местами( старый способ)
+// const temp = japanese; // временная переменная
+// japanese = vegetarion;
+// vegetarion = temp;
+// console.log(japanese, vegetarion);
+
+//// тоже самое с деструктуризацией
+// [vegetarion, japanese] = [japanese, vegetarion]
+//  console.log(japanese, vegetarion);
+
+const nestedArr = [11,2,[3,4]];
+// const [numb1, ,numb3] = nestedArr;
+// console.log(numb1,numb3);
+const [numb1, , [numb3, numb4]] = nestedArr   /// Вложеная деструктуризация!!!! распылили из масивы внутри массива
+console.log(numb1, numb3, numb4);
