8a1d3e304a6295a1d30905918af10cfa227d86cf
Yurii04UA <uyriparaka@gmail.com>
2022-04-26 14:41:35 +0300

    js

--- js/OOP/oop.js
+++ js/OOP/oop.js
@@ -8,9 +8,54 @@ const Person = function(firstName,birthYear){
 
 const yurii = new Person('yurii',1992)
 const yurii2 = new Person('yurii2',1990)
-console.log(yurii,yurii2);
+// console.log(yurii,yurii2);
 
 // 1.Создает пустой обьект {}
 // 2. Выхывается функция, this = {}
 // 3. {} связывается с прототипом
 // 4. Функция возвращает {}
+////////////////////////////////////////
+//Prototype 
+
+Person.prototype.printAge = function(){
+   console.log(new Date().getFullYear()-this.birthYear);
+}
+// yurii.printAge()
+// yurii2.printAge()
+
+////////
+// task
+
+const Car = function(name,speed){
+   this.name = name;
+   this.speed = speed;
+}
+
+Car.prototype.accelerate = function(){
+   this.speed = this.speed + 5
+   console.log(`${this.name} speed = ${this.speed} km `);
+}
+Car.prototype.breake = function(){
+   this.speed = this.speed -5;
+   console.log(`${this.name} speed = ${this.speed} km `);
+}
+
+const honda = new Car('honda',120)
+const bmw = new Car('bmw',150)
+// honda.accelerate()
+// honda.breake()
+// honda.accelerate()
+// honda.accelerate()
+// honda.breake()
+// honda.breake()
+// honda.breake()
+// honda.breake()
+
+// bmw.accelerate()
+// bmw.accelerate()
+// bmw.accelerate()
+// bmw.accelerate()
+
+
+///// Classes ES6
+
