8c5b5d634d9e043ce0aedc27b5a66e35330ec22d
Yurii04UA <uyriparaka@gmail.com>
2022-05-03 21:48:56 +0300

    js

--- js/cardio_workout/script.js
+++ js/cardio_workout/script.js
@@ -7,3 +7,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+if(navigator.geolocation){
+   navigator.geolocation.getCurrentPosition(
+      function (position){
+         const {latitude,longitude} = position.coords
+         console.log(latitude,longitude);
+         console.log(`https://www.google.com.ua/maps/@${latitude},${longitude},14z?hl=ru`);
+      },
+      function(){
+         alert("Ti huy")
+      }
+   )
+}
+
