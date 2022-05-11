ede31c82363c2c4637a66701e6c713a9cfaa8a72
Yurii04UA <uyriparaka@gmail.com>
2022-05-11 15:09:58 +0300

    js

--- js/All-about-ASYNC/async.js
+++ js/All-about-ASYNC/async.js
@@ -1,18 +1,47 @@
 "use strict";
-
+////////////////////////// XML REQUEST
 const btn = document.querySelector(".btn");
 const cardContainer = document.querySelector(".card");
 //////////////////////////////////////
 
-const getCountryData = (country) => {
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+// const getCountryData = (country) => {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
 
-  request.send();
-  request.addEventListener("load", function () {
-   const [data] = JSON.parse(this.responseText);
-   console.log(data);
-   const currencies = data.currencies;
+//   request.send();
+//   request.addEventListener("load", function () {
+//    const [data] = JSON.parse(this.responseText);
+
+//    const currencies = data.currencies;
+//    const currensyName = Object.values(currencies)[0].name;
+//    const languages = data.languages
+//    const language = Object.values(languages)[0]
+
+//    const html = `
+//    <article class="country">
+//      <img class="country__img" src="${data.flags.svg}" />
+//      <div class="country__data">
+//        <h3 class="country__name">${data.name.common}</h3>
+//        <h4 class="country__region">${data.region}</h4>
+//        <p class="country__row"><span>👨‍👩‍👧‍👦</span>${(
+//          +data.population / 1000000
+//        ).toFixed(2)} milions</p>
+//        <p class="country__row"><span>🗣️</span>${language}</p>
+//        <p class="country__row"><span>💰</span>${currensyName}</p>
+//      </div>
+//    </article>
+//    `;
+//     cardContainer.insertAdjacentHTML("beforeend", html);
+//     cardContainer.style.opacity = 1;
+//   });
+// };
+// getCountryData("ukraine");
+
+
+///////////////////////////////////
+
+const displayCountry = (data) => {
+  const currencies = data.currencies;
    const currensyName = Object.values(currencies)[0].name;
    const languages = data.languages
    const language = Object.values(languages)[0]
@@ -33,8 +62,22 @@ const getCountryData = (country) => {
    `;
     cardContainer.insertAdjacentHTML("beforeend", html);
     cardContainer.style.opacity = 1;
+}
+
+const getCountryAndBorderCountries = (country) => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+
+  request.send();
+  request.addEventListener("load", function () {
+   const [data] = JSON.parse(this.responseText);
+
+   displayCountry(data)
+   
+   // get border countries
+    const [first] = data.borders
+    console.log(first);
+    if(!first) return;
   });
 };
-getCountryData("ukraine");
-getCountryData("usa");
-
+getCountryAndBorderCountries("ukraine");
